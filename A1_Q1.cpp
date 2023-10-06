1e385b607e2383a9ccb4ecca93603cb9b7413f8b
palak <144807698+Palak-bansal-26@users.noreply.github.com>
2023-10-06 17:24:11 +0530

    Create A1_Q1.cpp

---
+++ A1_Q1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=n;j++)
+        {
+            if(i==0 || i+j==n-1 || i==n-1)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
